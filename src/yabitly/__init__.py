13264fd0256045049c65050b3873d9e44477be43
t0mkaka <dh_ashu@outlook.com>
2022-08-29 21:54:30 +0530

    Generating unique url on request

---
+++ src/yabitly/__init__.py
@@ -0,0 +1,52 @@
+import os
+
+from flask import Flask
+from flask import request
+
+BASE_URL = 'http://yabitly/'
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    from . import generateUrl
+    generateUrl.init_app(app)
+    # a simple page that says hello
+    @app.route('/generateUrl')
+    def generateURL():
+        url = request.args.get('url')
+        if url is None or len(url.strip()) == 0:
+            return  'Invalid Url : Please send proper URL', 400 
+
+        newUrl = BASE_URL + generateUrl.generateUrl(url)
+        return newUrl
+
+    # a simple page that says hello
+    @app.route('/redirect')
+    def redirect():
+        redirectUrl = None
+        return 'redirect To : {}'.format(redirectUrl)
+
+    return app
---
+++ src/yabitly/generateUrl.py
@@ -0,0 +1,44 @@
+""""
+
+This file has functions to generate unique ids for the url given. 
+
+Basically we want to generate a unique 64bit uuid and convert to hexa decimal 
+
+"""
+
+from uuid import uuid4
+import string 
+
+bit_size=64
+BASE_LIST = string.ascii_letters + string.digits
+BASE_DICT = dict((c, i) for i, c in enumerate(BASE_LIST))
+
+
+def init_app(app):
+    # app.teardown_appcontext(close_db)
+    # app.cli.add_command(init_db_command)
+    pass 
+
+def base_encode(integer, base=BASE_LIST):
+    if integer == 0:
+        return base[0]
+
+    length = len(base)
+    ret = ''
+    while integer != 0:
+        # print(integer % length)
+        ret = base[integer % length] + ret
+        integer = integer // length
+
+    return ret
+
+
+def generateUrl(url):
+    unique_id = uuid4().int >> bit_size
+    return base_encode(unique_id)
+    # print(unique_id)
+    # print(base_encode(unique_id))
+
+
+if __name__ =="__main__":
+    generateUrl('google.com')
